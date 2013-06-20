b63f23ee9a4d50ae0e7d3e6bab7763626ec12287
Jan Funke <funke@ini.ch>
2013-06-20 17:07:11 +0200

    install scripts still used old names

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # copy binary
-cp ./build/yanta ~/local/bin/splines
+cp ./build/yanta ~/local/bin/yanta
 
 # register icon
-# xdg-icon-resource install --context mimetypes --size 48 splines-file-type.png x-application-splines
+# xdg-icon-resource install --context mimetypes --size 48 yanta-file-type.png x-application-yanta
 
 # copy mime-type configuration
-cp splines-mime.xml ~/.local/share/mime/packages/application-x-splines.xml
+cp yanta-mime.xml ~/.local/share/mime/packages/application-x-yanta.xml
 
 # copy desktop entry
-cp splines.desktop ~/.local/share/applications/splines.desktop
+cp yanta.desktop ~/.local/share/applications/yanta.desktop
 
 # let the system process the changes
 update-mime-database ~/.local/share/mime
